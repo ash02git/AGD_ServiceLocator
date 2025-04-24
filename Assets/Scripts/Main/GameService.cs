10d0b6915c2212a957597cf4d66d251ef2e3c60e
Ashwin02 <ashwinabraham2001@gmail.com>
2025-04-24 17:42:29 +0530

    Converted all Singletons into ServiceLocator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,5 +1,10 @@
+using ServiceLocator.Events;
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +12,30 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; } //public get and private set. Anyone can get but everyone cannot set.
+    public SoundService soundService { get; private set; }
+    public WaveService waveService { get; private set; }
+    public EventService eventService { get; private set; }
+    public MapService mapService { get; private set; }
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private UIService uIService;
+    public UIService UIService=>uIService;
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+
+    [SerializeField ]private AudioSource audioEffects;
+    [SerializeField ]private AudioSource backgroundMusic;
 
     private void Start()
     {
+        eventService = new EventService();
+        UIService.SubscribeToEvents();
         playerService = new PlayerService(playerScriptableObject);
+        mapService = new MapService(mapScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        waveService = new WaveService(waveScriptableObject);
     }
 
     private void Update()
