905ffbf548a092925518e94cce4279993854ba17
Ashwin02 <ashwinabraham2001@gmail.com>
2025-04-24 15:40:47 +0530

    Created GameService as ServiceLocator and removed singleton of playerService

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,22 @@
+using ServiceLocator.Player;
+using ServiceLocator.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameService : GenericMonoSingleton<GameService>
+{
+    public PlayerService playerService { get; private set; } //public get and private set. Anyone can get but everyone cannot set.
+
+    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    private void Start()
+    {
+        playerService = new PlayerService(playerScriptableObject);
+    }
+
+    private void Update()
+    {
+        playerService.Update();
+    }
+}
